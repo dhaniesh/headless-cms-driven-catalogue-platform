8a5e9e1297e68eb4d38ae3550f0872beb8e4e5f7
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-11 23:55:54 +0530

    feat: integrate Contentful API and add dotenv for environment variables

--- index.ts
+++ index.ts
@@ -1,14 +1,27 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
+import "dotenv/config"
+import { string } from "zod";
 
 const app = express();
 const PORT = 3000;
+const contentful_base_url = `https://cdn.contentful.com/spaces/${process.env.CF_SPACE_ID}/environments/${process.env.CF_ENV_ID}/entries?`;
 
-app.get("/catalogue", (req: Request, res: Response) => {
-    res.status(200).json({
-        "success": true,
+app.get("/catalogue", async (req: Request, res: Response) => {
+    const header = {
+        "Authorization": `Bearer ${process.env.CF_DELIVERY_API_KEY}`
+    }
+    const contentful_catalogue_url = contentful_base_url + new URLSearchParams({
+        content_type: "product"
     })
+    // res.send(contentful_catalogue_url)
+    const response = await fetch(contentful_catalogue_url, {
+        method: "GET",
+        headers: header
+    })
+    const result = await response.json()
+    res.send(result)
 })
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log("Server running on port: ", PORT);
 })
