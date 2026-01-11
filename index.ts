55104974e3a053e8da18cad8a33d55fb430e9239
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-11 22:31:01 +0530

    express init

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import express, {Request, Response} from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/catalogue", (req: Request, res: Response) => {
+    res.status(200).json({
+        "success": true,
+    })
+})
+
+app.listen(PORT, ()=> {
+    console.log("Server running on port: ", PORT);
+})
