d1a93058e2767047b97f7b0bee9a912e133369ba
Dhaniesh Murugan <53963269+dhaniesh@users.noreply.github.com>
2026-01-13 00:18:27 +0530

    feat: add category endpoint and update catalogue with category filter

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import "dotenv/config"
-import { catalogue_query } from "./queries";
-import { productCollectionSchema } from "./types";
+import { catalogueQuery, category_query } from "./queries";
+import { categoryCollectionSchema, productCollectionSchema } from "./types";
 
 const app = express();
 const PORT = 3000;
@@ -10,12 +10,34 @@ const contentful_header = {
     "Authorization": `Bearer ${process.env.CF_DELIVERY_API_KEY}`
 }
 
-app.get("/catalogue", async (req: Request, res: Response) => {
+app.get("/category/", async (req: Request, res: Response) => {
     try {
         const response = await fetch(contentful_base_url, {
             method: "POST",
             headers: contentful_header,
-            body: JSON.stringify({ query: catalogue_query })
+            body: JSON.stringify({ query: category_query, })
+        })
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = categoryCollectionSchema.parse(await response.json())
+        const data = result.data.categoryCollection.items
+        res.json(data.map((item) => item.category))
+
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+        res.status(500).json({ error: 'Failed to fetch category' });
+    }
+})
+
+app.get("/catalogue/", async (req: Request, res: Response) => {
+    const { category } = req.query;
+    try {
+        const response = await fetch(contentful_base_url, {
+            method: "POST",
+            headers: contentful_header,
+            body: JSON.stringify({ query: catalogueQuery(String(category)) })
         })
 
         if (!response.ok) {
